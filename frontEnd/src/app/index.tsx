3fe7bed9b5a55e6ece1584eb11ff45b35dc0ae60
Kayky Ribeiro <kaykykaro04@gmail.com>
2024-09-10 20:04:45 -0300

    inicio

---
+++ frontEnd/src/app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View, ScrollView } from "react-native";import Constants from 'expo-constants'
+
+
+const statusBarHeight = Constants.statusBarHeight
+
+export default function Index() {
+  return (
+    <ScrollView  className="bg-slate-200" showsVerticalScrollIndicator={false}>
+      
+    </ScrollView>
+  );
+}
