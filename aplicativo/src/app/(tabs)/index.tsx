e8d151182e09a1f14ca815a2436c901fa27708d8
Kayky Ribeiro <kaykykaro04@gmail.com>
2024-09-22 16:59:23 -0300

    card

--- aplicativo/src/app/(tabs)/index.tsx
+++ aplicativo/src/app/(tabs)/index.tsx
@@ -5,23 +5,29 @@ const statusBarHeight = Constants.statusBarHeight;
 
 import { MenuButton } from '../../components/menu-button';
 import { Avatar } from '../../components/avatar';
+import { CardServic } from '../../components/cardServic';
 
 export default function Home() {
- return (
-   <View className='flex-1 p-4 bg-white'>
-    <View className='w-full h-auto flex-row items-center justify-between' style={{ paddingTop: statusBarHeight + 10}}>
-      <MenuButton />
-      <Avatar source={{ uri: 'https://github.com/orodrigo.png'}} size='small'/>
-    </View>
-    <View 
-      className='mt-5 border-b border-gray-300'
-    >
-      <View className='flex flex-row justify-between items-center mx-5 mb-2'>
-        <Text className='text-sm font-bold color-gray-500'>
-          Serviços em andamento
-        </Text>
+  return (
+    <View className='flex-1 p-4 bg-white'>
+      <View className='w-full h-auto flex-row items-center justify-between' style={{ paddingTop: statusBarHeight + 10 }}>
+        <MenuButton />
+        <Avatar source={{ uri: 'https://github.com/orodrigo.png' }} size='small' />
+      </View>
+      <View>
+        <View
+          className='mt-5 border-b border-gray-300'
+        >
+          <View className='flex flex-row justify-between items-center mx-5 mb-2'>
+            <Text className='text-sm font-bold color-gray-500'>
+              Serviços em andamento
+            </Text>
+          </View>
+        </View>
+      </View>
+      <View>
+        <CardServic />
       </View>
     </View>
-   </View>
   );
 }
--- aplicativo/src/components/cardServic.tsx
+++ aplicativo/src/components/cardServic.tsx
@@ -1,7 +1,31 @@
-import { View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { Pressable, View, Image, Text } from 'react-native';
 
 export  function CardServic() {
  return (
-   <View/>
+  <Pressable
+  className='bg-white h-auto rounded-lg m-5 px-2 py-1 flex flex-row justify-start shadow-lg shadow-blue-500/40 border-b border-blue-300 '
+>
+<View className='mr-2 flex justify-center items-center'>
+  <Image
+    className='w-16 h-16 rounded-full bg-black'
+  />
+</View>
+<View className='flex flex-grow justify-start gap-2'>
+  <View>
+    <Text className='text-xl font-semibold'>Celta vermelho 2009</Text>
+    <Text className='text-sm color-slate-500'>Pintura, polimento e Limpez interna</Text>
+  </View>
+  <View className='flex flex-row justify-start items-center'>
+    <Text className='text-md color-blue-500'>20/12/2024</Text>
+    <Text className='mx-2'>até</Text>
+    <Text className='text-md color-blue-500'>01/01/2025</Text>
+  </View>
+</View>
+<View className='flex flex-row justify-start items-center'>
+  <Ionicons name="arrow-forward-circle-outline" size={28} color="#3b82f6" />
+</View>
+  
+</Pressable>
   );
 }
